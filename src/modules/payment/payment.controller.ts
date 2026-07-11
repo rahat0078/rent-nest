850550abe7a8887d30a0ee6c86a0c0b232927296
rahat0078 <rarahat1710@gmail.com>
2026-07-11 13:50:43 +0600

    feat(res): payment response

--- src/modules/payment/payment.controller.ts
+++ src/modules/payment/payment.controller.ts
@@ -6,31 +6,30 @@ import { StatusCodes } from "http-status-codes";
 import config from "../../config";
 import { stripe } from "../../lib/stripe";
 
-const createPaymentCheckout = catchAsync(async (req: Request, res: Response) => {
-  const tenantId = req.user.id;
-  const { rentRequestId } = req.body;
-  const result = await paymentService.createPaymentCheckoutIntoDB(
-    tenantId,
-    rentRequestId,
-  );
-  sendResponse(res, {
-    success: true,
-    successCode: StatusCodes.OK,
-    message: "Payment session created successfully",
-    data: result,
-  });
-});
+const createPaymentCheckout = catchAsync(
+  async (req: Request, res: Response) => {
+    const tenantId = req.user.id;
+    const { rentRequestId } = req.body;
+    const result = await paymentService.createPaymentCheckoutIntoDB(
+      tenantId,
+      rentRequestId,
+    );
+    sendResponse(res, {
+      success: true,
+      successCode: StatusCodes.OK,
+      message: "Payment session created successfully",
+      data: result,
+    });
+  },
+);
 
 const createPaymentConfirm = async (request: Request, response: Response) => {
-
   let event = request.body;
 
   if (config.endpointSecret) {
-    
     const signature = request.headers["stripe-signature"]!;
 
     try {
-
       event = stripe.webhooks.constructEvent(
         request.body,
         signature,
@@ -40,11 +39,10 @@ const createPaymentConfirm = async (request: Request, response: Response) => {
       console.log("⚠️ Webhook signature verification failed.", err.message);
 
       return response.status(400).json({
-        message: err.message
+        message: err.message,
       });
     }
   }
-
 
   switch (event.type) {
     case "checkout.session.completed": {
@@ -54,11 +52,11 @@ const createPaymentConfirm = async (request: Request, response: Response) => {
 
       break;
     }
-  
+
     default:
       console.log(`Unhandled event type ${event.type}`);
   }
-  
+
   return response.sendStatus(200);
 };
 
@@ -66,12 +64,12 @@ const getMyAllPaymentHistory = catchAsync(
   async (req: Request, res: Response) => {
     const tenantId = req.user.id;
     const payment = await paymentService.getMyAllPaymentHistoryFromDB(tenantId);
-     sendResponse(res, {
-    success: true,
-    successCode: StatusCodes.OK,
-    message: "Payment history fetched successfully",
-    data: payment,
-  });
+    sendResponse(res, {
+      success: true,
+      successCode: StatusCodes.OK,
+      message: "Payment history fetched successfully",
+      data: payment,
+    });
   },
 );
 const getMySinglePayment = catchAsync(
